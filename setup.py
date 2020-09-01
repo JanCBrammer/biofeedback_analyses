9aa8e3413cecfae431380ba0786cdcd7d7607c24
JohnDoenut <jan.c.brammer@gmail.com>
2020-09-01 19:36:42 +0200

    Remove comma.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     author="Jan C. Brammer",
     author_email="jan.c.brammer@gmail.com",
-    packages=find_namespace_packages(exclude=[",misc", "literature"]),
+    packages=find_namespace_packages(exclude=["misc", "literature"]),
     python_requires=">=3.7",
     license="GPL-3.0",
 )
