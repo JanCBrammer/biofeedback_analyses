dc04992ca865e2b699db91bd36bac5bb77e90c7b
Jan C. Brammer <jan.c.brammer@gmail.com>
2020-09-03 18:06:36 +0200

    Started experimenting with streamlit dashboards.

---
+++ biofeedback_analyses/app_config.py
@@ -0,0 +1,5 @@
+SFREQ = 10
+DATADIR_PROCESSED = "C:/Users/JohnDoe/surfdrive/Biochill_RITE/20200818_v3.0.0/data/processed"
+DATADIR_RAW = "C:/Users/JohnDoe/surfdrive/Biochill_RITE/20200818_v3.0.0/data/raw"
+SUBJECTS = [f"subj-{str(i).zfill(2)}" for i in range(1, 9)]
+SESSIONS = [f"sess-{str(i).zfill(2)}" for i in range(1, 11)]
---
+++ streamlit_apps/plot_biofeedback.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+author: Jan C. Brammer <jan.c.brammer@gmail.com>
+"""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import numpy.ma as mask
+from pathlib import Path
+from biofeedback_analyses import resp_utils
+from bokeh.plotting import figure
+from bokeh.layouts import column
+from biofeedback_analyses.app_config import (SFREQ, DATADIR_PROCESSED, SUBJECTS, SESSIONS)
+
+st.beta_set_page_config(layout="wide")
+st.title("Slow Breathing Intensity")
+
+
+
+@st.cache
+def median_inst_amp(paths):
+
+    inst_amps = []
+
+    for path in paths:
+
+        data = pd.read_csv(path, sep="\t")
+        inst_amp = np.ravel(data["inst_amp"])
+        inst_amps.append(np.median(inst_amp))
+
+    return np.mean(inst_amps)
+
+subject = st.sidebar.selectbox("Select participant", SUBJECTS)
+
+physiopaths = list(Path(f"{DATADIR_PROCESSED}/{subject}").glob(f"{subject}*biofeedback*"))
+
+burst_threshold_low = median_inst_amp(physiopaths)
+burst_threshold_high = st.sidebar.number_input("Upper burst threshold (x * median amplitude all session)",
+                                               min_value=1., max_value=3., step=.5, value=1.5)
+burst_threshold_high *= burst_threshold_low
+burst_min_duration = st.sidebar.number_input("Minimum burst duration (seconds)", min_value=5, max_value=20, step=5)
+burst_min_duration = int(np.rint(burst_min_duration * SFREQ))
+
+plots = []
+for session in SESSIONS:    # plot all available sessions
+
+    physiopath_session = [path for path in physiopaths if session in str(path)]
+    if len(physiopath_session) != 1:
+        continue
+
+    title = str(physiopath_session[0].name[:22])
+    if title[-1] == "A":
+        title = title[:16] + "without_biofeedback"
+    elif title[-1] == "B":
+        title = title[:16] + "with_biofeedback"
+
+    data = pd.read_csv(physiopath_session[0], sep="\t")
+    resp_filt = np.ravel(data["resp_filt"])
+    inst_amp = np.ravel(data["inst_amp"])
+    sec = np.linspace(0, resp_filt.size / SFREQ, resp_filt.size)
+
+    bursts = resp_utils.bursts_dual_threshold(inst_amp, burst_threshold_low,
+                                              burst_threshold_high,
+                                              min_duration=burst_min_duration)
+
+    p = figure(tools="pan,box_zoom,wheel_zoom,reset", title=title,
+               x_axis_label="Seconds", y_axis_label="Amplitude")
+    p.line(sec, resp_filt, legend_label="breathing at 4 to 12 bpm")
+    p.line(sec, inst_amp, line_color="orange", legend_label="instantaneous amplitude")
+    p.line(mask.array(sec, mask=~bursts),
+           mask.array(inst_amp, mask=~bursts),
+           line_color="red", legend_label="bursts")    # mark bursts
+    p.ray(x=0, y=burst_threshold_low, line_color="green", legend_label="median amplitude all sessions")
+    p.ray(x=0, y=burst_threshold_high, line_color="green", line_dash="dashed", legend_label="upper burst threshold")
+
+    plots.append(p)
+
+st.bokeh_chart(column(*plots, sizing_mode="stretch_width"))
