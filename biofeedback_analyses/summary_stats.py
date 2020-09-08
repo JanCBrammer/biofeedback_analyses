c5f6aa573ae9592bdfbd8a518f96adb0cb30837c
Jan C. Brammer <jan.c.brammer@gmail.com>
2020-09-08 15:58:25 +0200

    Implemented computation of biofeedback summary stats.

--- biofeedback_analyses/summary_stats.py
+++ biofeedback_analyses/summary_stats.py
@@ -9,7 +9,7 @@ import pandas as pd
 from itertools import product
 from pathlib import Path
 from mne.io import read_raw_edf
-from biofeedback_analyses import event_utils, resp_utils, hrv_utils
+from biofeedback_analyses import resp_utils, hrv_utils
 from biofeedback_analyses.config import SUBJECTS, SESSIONS, SFREQ
 
 
@@ -40,6 +40,7 @@ def summary_instantiate(subject, inputs, outputs, recompute):
 
     computed = save_path.exists()   # Boolean indicating if file already exists.
     if computed and not recompute:    # only recompute if requested
+        print(f"Not overwriting {save_path}.")
         return
 
     rows = list(product(SUBJECTS, SESSIONS))
@@ -48,7 +49,10 @@ def summary_instantiate(subject, inputs, outputs, recompute):
     conditions = [row[1][-6:] for row in rows]
 
     d = {"subj": subjects, "sess": sessions, "cond": conditions,
-         "median_resp_amp": "", "median_resp_rate": ""}
+         "median_resp_amp": "", "median_resp_rate": "",
+         "normalized_median_resp_power": "", "n_bursts": "",
+         "mean_duration_bursts": "", "std_duration_bursts": "",
+         "percent_bursts": ""}
     df = pd.DataFrame(data=d)
     df.to_csv(save_path, sep="\t", index=False)
 
@@ -64,27 +68,89 @@ def summary_resp(subject, inputs, outputs, recompute):
 
     root = inputs["physio_path"][0]
     filename = inputs["physio_path"][1]
-    physio_paths = Path(root).joinpath(subject).glob(f"{subject}{filename}")
+    physio_paths = list(Path(root).joinpath(subject).glob(f"{subject}{filename}"))
 
-    for physio_path in physio_paths:
+    if not physio_paths:
+        print(f"No files found for {subject}.")
+        return
+
+    for i, physio_path in enumerate(physio_paths):
+
+        row_idx = get_row_idx(physio_path, df_summary)
+        columns = ["median_resp_amp", "median_resp_rate"]
+        computed = df_summary.loc[row_idx, columns].isna().values.sum() != len(columns)   # skip if all columns contain NaN (make sure to reserve NaN as place-holder for non-computed results)
+        if computed and not recompute:
+            print(f"Not re-computing {physio_path}.")
+            if i < len(physio_paths) - 1:    # make sure to save updates only when values have been (re-) computed
+                continue
+            else:
+                return
 
         data_raw = read_raw_edf(physio_path, preload=True, verbose="error")
         resp = np.ravel(data_raw.get_data(picks=[0]))
         resp_stats = resp_utils.compute_resp_stats(resp, SFREQ)
 
-        row_idx = get_row_idx(physio_path, df_summary)
-
         for key, value in resp_stats.items():
             df_summary.loc[row_idx, key] = value
 
-    df_summary.to_csv(save_path, sep="\t", index=False)
+        df_summary.to_csv(save_path, sep="\t", index=False)
+        print(f"Updated {save_path} with {physio_path}.")
 
-    print(f"Saved {save_path}")
+
+def summary_biofeedback(subject, inputs, outputs, recompute):
+
+    root = outputs["save_path"][0]
+    filename = outputs["save_path"][1]
+    save_path = Path(root).joinpath(f"{filename}")
+    df_summary = pd.read_csv(save_path, sep="\t")    # raises if file doesn't exist
+
+    root = inputs["physio_path"][0]
+    filename = inputs["physio_path"][1]
+    physio_paths = list(Path(root).joinpath(subject).glob(f"{subject}{filename}"))
+
+    if not physio_paths:
+        print(f"No files found for {subject}.")
+        return
+
+    burst_threshold_low = resp_utils.median_inst_amp(physio_paths)
+    burst_threshold_high = 2 * burst_threshold_low
+    burst_min_duration = int(np.rint(15 * SFREQ))
+
+    for i, physio_path in enumerate(physio_paths):
+
+        row_idx = get_row_idx(physio_path, df_summary)
+        columns = ["normalized_median_resp_power", "n_bursts",
+                   "mean_duration_bursts", "std_duration_bursts", "percent_bursts"]
+        computed = df_summary.loc[row_idx, columns].isna().values.sum() != len(columns)   # skip if all columns contain NaN (make sure to reserve NaN as place-holder for non-computed results)
+        if computed and not recompute:
+            print(f"Not re-computing {physio_path}.")
+            if i < len(physio_paths) - 1:    # make sure to save updates only when values have been (re-) computed
+                continue
+            else:
+                return
+
+        data = pd.read_csv(physio_path, sep="\t")
+        inst_amp = np.ravel(data["inst_amp"])
+
+        bursts = resp_utils.bursts_dual_threshold(inst_amp, burst_threshold_low,
+                                                  burst_threshold_high,
+                                                  min_duration=burst_min_duration)
+
+        burst_stats = resp_utils.compute_burst_stats(bursts, SFREQ)
+        for key, value in burst_stats.items():
+            df_summary.loc[row_idx, key] = value
+
+        biofeedback_stats = resp_utils.compute_biofeedback_stats(inst_amp,
+                                                                 normalize_by=burst_threshold_low)
+        for key, value in biofeedback_stats.items():
+            df_summary.loc[row_idx, key] = value
+
+        df_summary.to_csv(save_path, sep="\t", index=False)
+        print(f"Updated {save_path} with {physio_path}.")
 
 
 def summary_heart(subject, inputs, outputs, recompute):
     pass
 
 
-def summary_biofeedback(subject, inputs, outputs, recompute):
-    pass
+
