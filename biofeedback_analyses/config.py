556dc4f91c708d22f04865aee81a28725dacacd7
Jan C. Brammer <jan.c.brammer@gmail.com>
2020-09-30 09:51:30 +0200

    Switch to study dir.

--- biofeedback_analyses/config.py
+++ biofeedback_analyses/config.py
@@ -1,7 +1,7 @@
 SFREQ = 10
-DATADIR_PROCESSED = "C:/Users/JohnDoe/surfdrive/Biochill_RITE/20200818_v3.0.0/data/processed"
-DATADIR_RAW = "C:/Users/JohnDoe/surfdrive/Biochill_RITE/20200818_v3.0.0/data/raw"
-SUBJECTS = [f"subj-{str(i).zfill(2)}" for i in range(1, 9)]
+DATADIR_PROCESSED = "C:/Users/JohnDoe/surfdrive/Biochill_Study-1/data/processed"
+DATADIR_RAW = "C:/Users/JohnDoe/surfdrive/Biochill_Study-1/data/raw"
+SUBJECTS = [f"subj-{str(i).zfill(2)}" for i in range(1, 10)]
 sessions = [f"sess-{str(i).zfill(2)}" for i in range(1, 11)]
 conditions = ["cond-A", "cond-B", "cond-B", "cond-A", "cond-B", "cond-A", "cond-B", "cond-A", "cond-B", "cond-A"]
 SESSIONS = [f"{s}_{c}" for s, c in zip(sessions, conditions)]    # sessions and conditions are linked
